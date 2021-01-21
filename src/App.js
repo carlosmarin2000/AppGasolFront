5f3ade0ae40ffcaa34d57ba3566c1f6c878afc08
jhoanvasquez <64394129+jhoanvasquez@users.noreply.github.com>
2021-01-21 00:26:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,50 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { Component} from 'react';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import {IconConfig} from './Icon';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
-});
+function App() {
+    var datos
+    const [data, setData] = useState( [] );
 
-L.Marker.prototype.options.icon = DefaultIcon;
+    useEffect(async () => {
+      const result = await axios(
+        'http://localhost:3000',
+      );
+        
+      setData(result.data);
+      datos = result.data
+      console.log(result.data)
+    }, []);
+    
+  return (
+    
+    <div className="App">
+    {
+        data.map((dato, key) => {
+            return <div key={key}>{dato.longitud}</div>
+        })
+    }
+     <MapContainer center ={[4.083794705460288, -76.19565156780332]} zoom ={14}>
+     {
+        data.map((dato, key) => {
+            return <div key={key}>
+            <TileLayer 
+            url ="https://api.mapbox.com/styles/v1/carlosmarin2029/ckk07ngj9195318m8wc9ssocb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2FybG9zbWFyaW4yMDI5IiwiYSI6ImNrazA3cTN6dDBldHQyeHBlbXJmaGozb3UifQ.E2sV4SoTq_Iwqg3zlREv0w" 
+            />
+            <Marker position={[dato.longitud, dato.latitud]}  icon={IconConfig}>
+            </Marker></div>
+        })
+    }
+       
+     </MapContainer>
+    </div>
 
-const PuntoMap = ({arrayMarkers}) => {
-  return(
-    arrayMarkers.map((punto)=>(
-      <Marker position = {[punto.latitude, punto.longitude]}>
-        <Popup>
-          {punto.name}. <br/>
-          {punto.address}. <br/>
-          {punto.phone}. <br/>
-        </Popup>
-      </Marker>
-    ))
-  )
-};
-
-class App extends Component {    
-  
-  state = {
-    arrayMarkers:[]
-  };
-
-  componentDidMount() {
-    fetch("http://localhost:3000/",{
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-                'Content-Type':'application/json'
-      }
-    })
-    .then(request => request.json())
-    .then((dataJSON) => {
-      console.log(dataJSON)
-      this.setState({ arrayMarkers: dataJSON })
-    })
-    .catch(console.log)
-  };
-
-  render(){
-    return (
-      <div className="App">
-      <MapContainer center ={[4.083794705460288, -76.19565156780332]} zoom ={14}>
-        <TileLayer 
-        url ="https://api.mapbox.com/styles/v1/carlosmarin2029/ckk07ngj9195318m8wc9ssocb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2FybG9zbWFyaW4yMDI5IiwiYSI6ImNrazA3cTN6dDBldHQyeHBlbXJmaGozb3UifQ.E2sV4SoTq_Iwqg3zlREv0w" 
-        />
-        <PuntoMap  arrayMarkers = {this.arrayMarkers} />
-      </MapContainer>
-      </div>
-    );
-  }
+    
+  );
 }
 
 export default App;
